f8b8e93cef9c05017533ff3e28d1555b7aafd242
Cole Spitzer <Cole.Spitzer@bbrown.com>
2026-09-04 01:51:25 -0400

    Saving Changes need to add fix for hero carousel

---
+++ wwwroot/js/theme.js
@@ -0,0 +1,9 @@
+﻿window.themeInterop = {
+    getTheme: function () {
+        return document.documentElement.getAttribute('data-theme') || 'dark';
+    },
+    setTheme: function (theme) {
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
+    }
+};
